b6c17fe974523967f04bfdb45f6345b341196a91
salehi-s <salehi.s@husky.neu.edu>
2021-03-08 18:53:02 -0500

    Add module-reducer.js (init) and declare initialState and moduleReducer

---
+++ src/reducers/module-reducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+    modules: [
+        {title: "CS5610", _id: "1"},
+        {title: "DS5110", _id: "2"},
+        {title: "CS5200", _id: "3"}
+    ]
+}
+
+const moduleReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "CREATE_MODULE":
+
+        case "FIND_MODULES_FOR_COURSE":
+
+        case "FIND_MODULE":
+
+        case "UPDATE_MODULE":
+
+        case "DELETE_MODULE":
+
+    }
+}
