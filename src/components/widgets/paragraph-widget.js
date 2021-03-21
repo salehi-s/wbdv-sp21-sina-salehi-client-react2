ab844fcc3b0bb37a0d569c2790d260732ac135af
salehi-s <salehi.s@husky.neu.edu>
2021-03-21 00:51:56 -0400

    Allow for Dynamic Fetching and Rendering of Widgets (Heading Widget and Paragraph Widget) in Widget List (as opposed to rendering dummy data)

--- src/components/widgets/paragraph-widget.js
+++ src/components/widgets/paragraph-widget.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const ParagraphWidget = ({widget}) =>
-    <div>
-        <p>Paragraph Widget {widget.id}</p>
-    </div>
+const ParagraphWidget = ({widget, setWidget, editing}) => {
+    return (
+        <div>
+            {
+                editing &&
+                    <textarea className = "form-control"
+                              value = {widget.text}
+                              onChange = {(e) => setWidget({...widget,
+                                                                                        text: e.target.value})}></textarea>
+            }
+            {
+                !editing &&
+                    <p>
+                        {widget.text}
+                    </p>
+            }
+        </div>
+    )
+}
 
 export default ParagraphWidget
--- src/components/widgets/widget-list.js
+++ src/components/widgets/widget-list.js
@@ -1,33 +1,110 @@
 import React, {useState, useEffect} from 'react'
+import {connect} from 'react-redux'
 import {useParams} from 'react-router-dom'
 
+import widgetService from '../../services/widget-service'
 import HeadingWidget from './heading-widget'
 import ParagraphWidget from './paragraph-widget'
 
-const WidgetList = () => {
-    const {topicId} = useParams()
+const WidgetList = ({
+    // allWidgets = [],
+    // createWidget,
+    // findWidgetsForTopic,
+    // findWidget,
+    // updateWidget,
+    // deleteWidget
+}) => {
+    const {
+        // layout,
+        // courseId,
+        // moduleId,
+        // lessonId,
+        topicId
+        // widgetId
+    } = useParams()
+
     const [widgets, setWidgets] = useState([])
+    const [widget, setWidget] = useState({})
+
     useEffect(() => {
         fetch(`http://localhost:8080/api/topics/${topicId}/widgets`)
             .then(response => response.json())
             .then(widgets => setWidgets(widgets))
     }, [topicId])
+
+    const createWidget = () => {
+        fetch(`http://localhost:8080/api/topics/${topicId}/widgets`, {
+            method: "POST",
+            body: JSON.stringify({
+                type: "HEADING",
+                size: "2",
+                text: "New Widget"
+            }),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(widget => setWidgets((widgets) => [...widgets,
+                                                          widget]))
+    }
+
+    const deleteWidget = (id) =>
+        fetch(`http://localhost:8080/api/widgets/${id}`, {
+            method: "DELETE"
+        })
+            .then((status) => {
+                setWidgets((widgets) => widgets.filter(w => w.id !== id))
+            })
+
+    const updateWidget = (id, widget) =>
+        fetch(`http://localhost:8080/api/widgets/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(widget),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then((status) => {
+                setWidget({})
+                setWidgets((widgets) => widgets.map(w => w.id === id ? widget : w))
+            })
+
     return (
         <div>
-            <i className = "fas fa-plus fa-2x float-right"></i>
-            <h1>Widget List</h1>
+            <i className = "fas fa-plus fa-2x float-right"
+               onClick = {createWidget}></i>
+            <h1>Widget List {widget.id}</h1>
             <ul className = "list-group">
                 {
-                    widgets.map(widget =>
+                    widgets.map(_widget =>
                         <li className = "list-group-item"
-                            key = {widget.id}>
+                            key = {_widget.id}>
                             {
-                                widget.type === "HEADING" &&
-                                    <HeadingWidget widget = {widget}/>
+                                _widget.id === widget.id &&
+                                    <>
+                                        <i className = "fas fa-trash fa-2x float-right"
+                                           onClick = {() => deleteWidget(_widget.id)}></i>
+                                        <i className = "fas fa-check fa-2x float-right"
+                                           onClick = {() => {updateWidget(_widget.id, widget)}}></i>
+                                    </>
                             }
                             {
-                                widget.type === "PARAGRAPH" &&
-                                    <ParagraphWidget widget = {widget}/>
+                                _widget.id !== widget.id &&
+                                    <i className = "fas fa-cog fa-2x float-right"
+                                       onClick = {() => setWidget(_widget)}></i>
+                            }
+                            {
+                                _widget.type === "HEADING" &&
+                                    <HeadingWidget widget = {widget}
+                                                   setWidget = {setWidget}
+                                                   editing = {_widget.id === widget.id}/>
+                            }
+                            {
+                                _widget.type === "PARAGRAPH" &&
+                                    <ParagraphWidget widget = {widget}
+                                                     setWidget = {setWidget}
+                                                     editing = {_widget.id === widget.id}/>
                             }
                         </li>
                     )
@@ -36,5 +113,51 @@ const WidgetList = () => {
         </div>
     )
 }
+
+/*
+{/!* State to Property Mapper *!/}
+const stpm = (state) => ({
+    widgets: state.widgetReducer.widgets
+})
+
+const dtpm = (dispatch) => ({
+    createWidget: (tid) => {
+        widgetService.createWidget(tid, {
+            type: "HEADING"
+        })
+            .then(widget => dispatch({
+                type: "CREATE_WIDGET",
+                widget: widget
+            }))
+    },
+    findWidgetsForTopic: (tid) => {
+        widgetService.findWidgetsForTopic(tid)
+            .then(widgets => dispatch({
+                type: "FIND_ALL_WIDGETS_FOR_TOPIC",
+                widgets: widgets
+            }))
+    },
+    findWidget: () => {
+        dispatch({type: "FIND_WIDGET"})
+    },
+    updateWidget: (newItem) => {
+        widgetService.updateWidget(newItem._id, newItem)
+            .then(status => dispatch({
+                type: "UPDATE_WIDGET",
+                updatedWidget: newItem
+            }))
+    },
+    deleteWidget: (itemToDelete) => {
+        widgetService.deleteWidget(itemToDelete._id)
+            .then(status => dispatch({
+                type: "DELETE_WIDGET",
+                widgetToDelete: itemToDelete
+            }))
+    }
+})
+
+export default connect(stpm, dtpm)
+(WidgetList)
+*/
 
 export default WidgetList
