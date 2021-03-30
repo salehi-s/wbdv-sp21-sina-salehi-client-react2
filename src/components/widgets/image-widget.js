3b2ddd6d10642c79fcde479d1bdde2a84a18ab21
salehi-s <salehi.s@husky.neu.edu>
2021-03-30 15:34:50 -0400

    Add editing/!editing rendering to List Widget and Image Widget

--- src/components/widgets/image-widget.js
+++ src/components/widgets/image-widget.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const ImageWidget = () => {
+const ImageWidget = ({widget, setWidget, editing}) => {
     return(
         <div className = "container-fluid">
             <h2>Image Widget</h2>
+            {
+                editing &&
+                    <div className = "container-fluid">
+                        URL
+                        <input className = "form-control"
+                               value = {widget.url}/>
+                        Width
+                        <input className = "form-control"
+                               value = {widget.width}/>
+                        Height
+                        <input className = "form-control"
+                               value = {widget.height}/>
+                    </div>
+            }
         </div>
     )
 }
--- src/components/widgets/list-widget.js
+++ src/components/widgets/list-widget.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const ListWidget = () => {
+const ListWidget = ({widget, setWidget, editing}) => {
     return(
         <div className = "container-fluid">
             <h2>List Widget</h2>
+            {
+                editing &&
+                    <div className = "container-fluid">
+                        <input type = "checkbox"/> Ordered
+                        <br/>
+                        List Items
+                        <textarea className = "form-control"
+                                  value = {widget.text}
+                                  rows = {10}>
+
+                        </textarea>
+                    </div>
+            }
+            {
+                !editing &&
+                    <ul>
+                        {
+                            widget.text.split("\n").map(item => {
+                                return(
+                                    <li>{item}</li>
+                                )
+                            })
+                        }
+                    </ul>
+            }
         </div>
     )
 }
