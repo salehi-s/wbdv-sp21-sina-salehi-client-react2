bdd751e0b4fdfde02d18620d1abb1ed3b418805e
salehi-s <salehi.s@husky.neu.edu>
2021-04-12 19:57:23 -0400

    Update True False Question Such That Correct Answers are Highlighted in Green and Incorrect Answers are Highlighted in Red

--- src/components/quizzes/questions/true-false-question.js
+++ src/components/quizzes/questions/true-false-question.js
@@ -13,12 +13,12 @@ const TrueFalseQuestion = ({question}) => {
                     {
                         grade &&
                         JSON.stringify(answer) === question.correct &&
-                            <i className = "fas fa-check fa-2x float-right wbdv-editable-item-action-icon"/>
+                            <i className = "fas fa-check fa-2x float-right wbdv-editable-item-action-icon wbdv-answer-correct"/>
                     }
                     {
                         grade &&
                         JSON.stringify(answer) !== question.correct &&
-                            <i className = "fas fa-times fa-2x float-right wbdv-editable-item-action-icon"/>
+                            <i className = "fas fa-times fa-2x float-right wbdv-editable-item-action-icon wbdv-answer-incorrect"/>
                     }
                 </h4>
             </div>
@@ -59,22 +59,98 @@ const TrueFalseQuestion = ({question}) => {
                 grade &&
                     <div className = "container-fluid">
                         <ul className = "list-group">
-                            <li className = {`list-group-item ${JSON.stringify(answer) === question.correct ? "list-group-item-success" : "list-group-item-danger"}`}>
-                                <label>
-                                    <input type = "radio"
-                                           name = {question._id}
-                                           onClick = {() => setAnswer(true)}/> True
-                                    <i className = {`fas ${JSON.stringify(answer) === question.correct ? "fa-check" : "fa-times"} fa-2x float-right wbdv-editable-item-action-icon`}/>
-                                </label>
-                            </li>
-                            <li className = {`list-group-item ${JSON.stringify(answer) === question.correct ? "list-group-item-success" : "list-group-item-danger"}`}>
-                                <label>
-                                    <input type = "radio"
-                                           name = {question._id}
-                                           onClick = {() => setAnswer(false)}/> False
-                                    <i className = {`fas ${JSON.stringify(answer) === question.correct ? "fa-check" : "fa-times"} fa-2x float-right wbdv-editable-item-action-icon`}/>
-                                </label>
-                            </li>
+                            {
+                                JSON.stringify(answer) === question.correct &&
+                                    <>
+                                        {
+                                            question.correct === "true" &&
+                                                <>
+                                                    <li className = "list-group-item list-group-item-success">
+                                                        <label>
+                                                            <input type = "radio"
+                                                                   name = {question._id}
+                                                                   onClick = {() => setAnswer(true)}/> True
+                                                            <i className = "fas fa-check fa-2x float-right wbdv-editable-item-action-icon"/>
+                                                        </label>
+                                                    </li>
+                                                    <li className = "list-group-item">
+                                                        <label>
+                                                            <input type = "radio"
+                                                                   name = {question._id}
+                                                                   onClick = {() => setAnswer(false)}/> False
+                                                        </label>
+                                                    </li>
+                                                </>
+                                        }
+                                        {
+                                            question.correct === "false" &&
+                                                <>
+                                                    <li className = "list-group-item">
+                                                        <label>
+                                                            <input type = "radio"
+                                                                   name = {question._id}
+                                                                   onClick = {() => setAnswer(true)}/> True
+                                                        </label>
+                                                    </li>
+                                                    <li className = "list-group-item list-group-item-success">
+                                                        <label>
+                                                            <input type = "radio"
+                                                                   name = {question._id}
+                                                                   onClick = {() => setAnswer(false)}/> False
+                                                            <i className = "fas fa-check fa-2x float-right wbdv-editable-item-action-icon"/>
+                                                        </label>
+                                                    </li>
+                                                </>
+                                        }
+                                    </>
+                            }
+                            {
+                                !(JSON.stringify(answer) === question.correct) &&
+                                    <>
+                                        {
+                                            question.correct === "true" &&
+                                                <>
+                                                    <li className = "list-group-item list-group-item-success">
+                                                        <label>
+                                                            <input type = "radio"
+                                                                   name = {question._id}
+                                                                   onClick = {() => setAnswer(true)}/> True
+                                                            <i className = "fas fa-check fa-2x float-right wbdv-editable-item-action-icon"/>
+                                                        </label>
+                                                    </li>
+                                                    <li className = "list-group-item list-group-item-danger">
+                                                        <label>
+                                                            <input type = "radio"
+                                                                   name = {question._id}
+                                                                   onClick = {() => setAnswer(false)}/> False
+                                                            <i className = "fas fa-times fa-2x float-right wbdv-editable-item-action-icon"/>
+                                                        </label>
+                                                    </li>
+                                                </>
+                                        }
+                                        {
+                                            question.correct === "false" &&
+                                                <>
+                                                    <li className = "list-group-item list-group-item-danger">
+                                                        <label>
+                                                            <input type = "radio"
+                                                                   name = {question._id}
+                                                                   onClick = {() => setAnswer(true)}/> True
+                                                            <i className = "fas fa-times fa-2x float-right wbdv-editable-item-action-icon"/>
+                                                        </label>
+                                                    </li>
+                                                    <li className = "list-group-item list-group-item-success">
+                                                        <label>
+                                                            <input type = "radio"
+                                                                   name = {question._id}
+                                                                   onClick = {() => setAnswer(false)}/> False
+                                                            <i className = "fas fa-check fa-2x float-right wbdv-editable-item-action-icon"/>
+                                                        </label>
+                                                    </li>
+                                                </>
+                                        }
+                                    </>
+                            }
                         </ul>
                         <br/>
                         <div className = "container-fluid">
